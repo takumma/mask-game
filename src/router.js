32cf8540b3b9a17534aa2ea595a9d37f78c0737c
shoma <h30i16@ed.cc.suzuka-ct.ac.jp>
2020-04-09 13:51:09 +0900

    ✨ : Implement files
    vue-routerの導入。画面遷移ができるようになった

---
+++ src/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import GameView from './pages/GameView.vue'
+import HomeView from './pages/HomeView.vue'
+import ResultView from './pages/ResultView.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+        path: '/game',
+        name: 'game',
+        component: GameView
+    },
+    {
+        path: '/result',
+        name: 'result',
+        component: ResultView
+    }
+  ]
+})
