641b2d05aa3d498bcc422df14ac7b72c58dbb3b7
shoma <h30i16@ed.cc.suzuka-ct.ac.jp>
2020-04-09 13:50:03 +0900

    🎨 : Implement files
    メインとなるvueファイルの追加

---
+++ src/pages/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="home">
+    <h1>HomeView</h1>
+    <router-link to="/game">Go to Game</router-link>
+    <router-link to="/result">Go to Result</router-link>
+  </div>
+</template>
