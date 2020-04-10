26e76576659f6b01d1f5fb0dd58b13170c60bd37
shoma <h30i16@ed.cc.suzuka-ct.ac.jp>
2020-04-10 17:22:42 +0900

    ✨ : Add
    tweet機能の実装完了

--- src/components/ResultStage.vue
+++ src/components/ResultStage.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="inner-result">
-    <h1 id="result-title">{{num}}人にマスクをつけることができました！</h1>
+    <p id="result-title"><span id="result-num">{{num}}</span>人に<br>マスクをつけることができました！</p>
     <button class="result-button" @click="toHome">HOME</button>
     <button class="result-button" @click="RetryGame">Retry</button>
     <br>
-    <button class="result-button">つぶやく</button>
+    <button class="result-button" @click="tweet()">つぶやく</button>
     <!--これだけでつぶやけちゃう-->
     <!--<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>-->
   </div>
@@ -16,7 +16,7 @@ export default {
   name: 'ResultStage',
   data(){
     return{
-      num : 50
+      num : 50,
     }
   },
   methods: {
@@ -25,6 +25,13 @@ export default {
     },
     RetryGame(){
       this.$router.push('/game')
+    },
+    tweet(){
+      const text = 'Maskgameで'+this.num+'人にマスクをつけました'
+      //urlはローンチするときに変更。あとハッシュタグもアプリ名に変更する
+      const url = 'https://shoma-profile.netlify.com'
+      const tweeturl = 'https://twitter.com/share?text='+text+'&url='+url+'&hashtags=maskgame'
+      window.open(tweeturl,'sampletweet')
     }
   }
 }
@@ -36,21 +43,27 @@ export default {
   top: 50%;
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
-  width: 90%;
-  height: 90%;
+  width: 85%;
+  height: 92%;
   background-color: whitesmoke;
-  border-radius: 50px;
+  border-radius: 30px;
 }
 
 #result-title{
-  padding-top: 10%;
-  margin-top: 0;
+  margin-top: 20%;
+}
+
+#result-num{
+  font-size: 50px;
+  font-weight: 700;
 }
 
 .result-button{
   border: limegreen 3px solid;
   border-radius: 5px;
   padding: 10px;
+  width: 30%;
+  margin: 10px ;
 }
 </style>
 
