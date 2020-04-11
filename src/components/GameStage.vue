dba268dfb5fdd9192e5cc012250a8952e1342610
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-04-11 23:22:06 +0900

    human.vueの追加、gameStageにランダムな位置に人が出るようにした

--- src/components/GameStage.vue
+++ src/components/GameStage.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>
+  <div id="inner-game">
     <h1>GameView</h1>
     <router-link to="/">Go to Home</router-link>
     <router-link to="/result">Go to Result</router-link>
+    {{ random() }} <br>
+    <human :top="random()" :left="random()"/>
   </div>
 </template>
+
+<script>
+import human from './Human'
+export default {
+  name: 'GameStage',
+  components: {human},
+  methods: {
+    random() {
+      return Math.floor(Math.random() * 501) + 'px'
+    },
+  }
+}
+</script>
+
+<style scoped>
+#inner-game{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  width: 85%;
+  height: 92%;
+  background-color: whitesmoke;
+  border-radius: 30px;
+}
+</style>
---
+++ src/components/Human.vue
@@ -0,0 +1,37 @@
+<template>
+  <dev id="human" :style="styles">
+    human {{top + " " + left + " " }}
+  </dev>
+</template>
+
+<script>
+export default {
+  name: 'human',
+  props: {
+    top: {},
+    left: {}
+  },
+	computed: {
+    styles() {
+      return {
+        '--top': this.top,
+        '--left': this.left
+      }
+    }
+	}
+}
+</script>
+
+<style scoped>
+#human {
+  --top: 0px;
+  --left: 0px;
+
+  position: absolute;
+  top: var(--top);
+  left: var(--left);
+
+	background-color: red;
+	padding: 20px;
+}
+</style>
