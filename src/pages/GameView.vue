0aa383d1bc41beb78f01b01bd4baf467abd53461
shoma <h30i16@ed.cc.suzuka-ct.ac.jp>
2020-04-12 09:49:32 +0900

    idを消去

--- src/pages/GameView.vue
+++ src/pages/GameView.vue
@@ -1,13 +1,12 @@
 <template>
-  <div id="game">
-    <game-stage/>
+  <div>
+    <game-stage/>	
   </div>
 </template>
 
 
 <script>
 import GameStage from '@/components/GameStage'
-import 'normalize.css'
 export default {
   name: 'GameView',
   components: { GameStage },
