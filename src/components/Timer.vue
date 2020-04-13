a1032c461f608eed849e235aedf05bd98798100a
shoma <h30i16@ed.cc.suzuka-ct.ac.jp>
2020-04-13 19:49:02 +0900

    finish->0に変更

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -3,7 +3,7 @@
       <countdown :left-time="60000" :auto-start="true" ref="cnt">
         <div slot="process" slot-scope="anyYouWantedScopName">{{ `カウント: ${anyYouWantedScopName.timeObj.ceil.s}` }}</div>
         <div slot="finish">
-          <span>finish</span>
+          <span>0</span>
         </div>
       </countdown>
     </div>
